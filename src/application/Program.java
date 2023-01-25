25427293f4f3bd28213d7aba6c2ded2983e70742
Eder <edermartelinho@hotmail.com>
2023-01-25 01:03:43 -0300

    FileReader / BufferedReader / Bloco try-with-resources

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,34 +1,36 @@
 package application;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
+
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-//lendo arquivo
+		//FileReader / BufferedReader / Bloco try-with-resources
 		
-	File file = new File("c:\\temp\\out.txt");//exemplo de caminho
-	Scanner sc = null;
-	try {
-		sc = new Scanner(file);
-		while (sc.hasNextLine()) {
-			System.out.println(sc.nextLine());
-		}
 		
-	  }
-	  catch (IOException e) {
-		  System.out.println("Error: " + e.getMessage());
-	  }
-	  finally {
-		  if (sc != null) {
-			  sc.close();
-		  }
-	  
-	  }
+				String path = "c:\\temp\\in.txt";
+				
+				
+				try(BufferedReader br = new BufferedReader(new FileReader(path))){
+					String line = br.readLine();
+					
+					while (line != null) {
+						System.out.println(line);
+						line = br.readLine();
+					}
+					
+				}
+				catch (IOException e) {
+					System.out.println("Error: " + e.getMessage());
+				} 
 	}
 }
+			
+				
 		
 		
