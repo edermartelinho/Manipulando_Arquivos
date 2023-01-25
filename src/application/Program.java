a0a25ed37a88f0a84de6f6854ed390e754f82ad9
Eder <edermartelinho@hotmail.com>
2023-01-25 00:44:19 -0300

    Lendo arquivos

---
+++ src/application/Program.java
@@ -0,0 +1,34 @@
+package application;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+//lendo arquivo
+		
+	File file = new File("c:\\temp\\out.txt");//exemplo de caminho
+	Scanner sc = null;
+	try {
+		sc = new Scanner(file);
+		while (sc.hasNextLine()) {
+			System.out.println(sc.nextLine());
+		}
+		
+	  }
+	  catch (IOException e) {
+		  System.out.println("Error: " + e.getMessage());
+	  }
+	  finally {
+		  if (sc != null) {
+			  sc.close();
+		  }
+	  
+	  }
+	}
+}
+		
+		
