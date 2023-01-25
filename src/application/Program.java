f5db8c903798051ffaf864032d088e55e0060201
Eder <edermartelinho@hotmail.com>
2023-01-25 01:14:46 -0300

    Criar/recriar arquivo- new FileWriter(path)   - Acrescenta ao arquivo existente- new FileWriter(path,true)   - BufferedWriter(mais rapido)

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,12 +3,35 @@ package application;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 
 public class Program {
 
 	public static void main(String[] args) {
+		/*		
+		//lendo arquivo
+				
+			 File file = new File("c:\\temp\\in.txt");
+			Scanner sc = null;
+			try {
+				sc = new Scanner(file);
+				while (sc.hasNextLine()) {
+					System.out.println(sc.nextLine());
+				}
+				
+			  }
+			  catch (IOException e) {
+				  System.out.println("Error: " + e.getMessage());
+			  }
+			  finally {
+				  if (sc != null) {
+					  sc.close();
+				  }
+			  }*/
+				
+		/*
 		
 		//FileReader / BufferedReader / Bloco try-with-resources
 		
@@ -28,6 +51,25 @@ public class Program {
 				catch (IOException e) {
 					System.out.println("Error: " + e.getMessage());
 				} 
+				*/
+		
+		
+ 	String[] lines = new String[] {"criando string dentro do arquivo", "java", "java"};
+		
+		String path = "c:\\temp\\out.txt";
+		
+		     // para nao desfazer acrescentar basta colocar o true- (path, true)
+		
+		try  (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))){
+			for (String line : lines) {
+				bw.write(line);
+				bw.newLine();
+			}
+		}
+		catch (IOException e) {
+		 e.printStackTrace();
+		} 
+		
 	}
 }
 			
